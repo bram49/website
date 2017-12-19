7cd1c66c06b9a950f4cfbd182905e77c779f89e1
bram49 <schut49@gmail.com>
2017-12-19 11:49:41 +0100

    update1

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\s163756\Desktop\DjangoServer\website\website\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
